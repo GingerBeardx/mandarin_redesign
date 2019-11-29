f3ec1552b23e4bb48d862b455fbf9a740e69f4cd
GingerBeardx <ecryptographer@gmail.com>
2019-11-29 12:45:52 -0700

    parsing item data

--- get_data.py
+++ get_data.py
@@ -20,3 +20,26 @@ with urlReq.urlopen('http://mandaringarden.biz/lunch.html') as f:
 # Convert the Specials page into soup to prepare for parsing
 soup = bs(specials_page, 'html.parser')
 selections = soup.find(id='lunchlist')
+menu_items = selections.find_all('dl')
+
+# Loop over the menu items and parse the info
+# Items to look for in the menu items:
+#   Number, Hot, Item, Price
+item = menu_items[0].find_all('dd')
+number = ''
+hot = False
+item_name = ''
+price = ''
+for parts in item:
+    if parts['class'] == ['number']:
+        number = parts.text
+    if parts['class'] == ['item']:
+        item_name = parts.text
+    if parts['class'] == ['price']:
+        price = parts.text
+
+print(
+    f'Info gathered - Number: {number} - Hot: {hot} - Item: {item_name} - Price: {price}')
+
+
+# Pass parsed info into a .csv file

26fcd1c7a3a1665dadd5f1bc476bcaa4d56ae56f
GingerBeardx <ecryptographer@gmail.com>
2019-11-29 13:04:44 -0700

    successfully scraped lunch specials

--- get_data.py
+++ get_data.py
@@ -25,21 +25,25 @@ menu_items = selections.find_all('dl')
 # Loop over the menu items and parse the info
 # Items to look for in the menu items:
 #   Number, Hot, Item, Price
-item = menu_items[0].find_all('dd')
-number = ''
-hot = False
-item_name = ''
-price = ''
-for parts in item:
-    if parts['class'] == ['number']:
-        number = parts.text
-    if parts['class'] == ['item']:
-        item_name = parts.text
-    if parts['class'] == ['price']:
-        price = parts.text
+for item_list in menu_items:
+    item = item_list.find_all('dd')
+    number = ''
+    hot = False
+    item_name = ''
+    price = ''
+    item = item_list.find_all('dd')
+    for parts in item:
+        if parts['class'] == ['number']:
+            number = parts.text
+            if parts.img is not None:
+                hot = True
+        if parts['class'] == ['item']:
+            item_name = parts.text.lower()
+        if parts['class'] == ['price']:
+            price = parts.text
 
-print(
-    f'Info gathered - Number: {number} - Hot: {hot} - Item: {item_name} - Price: {price}')
+    print(
+        f'Info gathered - Number: {number} - Hot: {hot} - Item: {item_name} - Price: {price}')
 
 
 # Pass parsed info into a .csv file

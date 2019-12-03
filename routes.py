7107f10b7390d403e3eecbabd55a4ff87cb02d52
GingerBeardx <ecryptographer@gmail.com>
2019-12-02 20:06:28 -0700

    Started luncheon specials page, items generated, spicy items specified

--- routes.py
+++ routes.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/specials')
+def specials():
+    with open('./scrape/lunch_specials.csv', 'r') as f:
+        specials = csv.DictReader(f)
+        return render_template('specials.html', specials=specials)
 
 if __name__ == '__main__':
     app.run(debug=True)
